febf9665870481e3b87158406d817fdf85be5f63
linq <linq@2tx.com>
2022-09-08 14:59:30 +0800

    通信类

--- code/SecKeyClient/SecKeyClient/main.cpp
+++ code/SecKeyClient/SecKeyClient/main.cpp
@@ -1,7 +1,28 @@
 ﻿#include <cstdio>
+#include "TcpSocket.h"
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
 
 int main()
 {
-    printf("%s 向你问好!\n", "SecKeyClient");
-    return 0;
+    TcpSocket* op = new TcpSocket;
+	//服务器一定要先启动，不然会出错
+    op->connectToHost("127.0.0.1", 6666);
+    cout << "连接成功 " << endl;
+
+	// 通信
+	while (1)
+	{
+		string sendmsg = "hello server ...";
+		cout << "发送数据" << sendmsg << endl;
+		op->sendMsg(sendmsg);
+		// 接收数据
+		int recvLen = -1;
+		string recvMsg = op->recvMsg();
+		cout << "recvmsg: " << recvMsg << endl;
+		sleep(1);
+	}
+	return 0;
 }
--- code/SecKeyServer/SecKeyServer/main.cpp
+++ code/SecKeyServer/SecKeyServer/main.cpp
@@ -1,7 +1,42 @@
 ﻿#include <cstdio>
+#include "TcpServer.h"
+#include <iostream>
+#include <pthread.h>
+using namespace std;
 
+void* callback(void* arg)
+{
+    TcpSocket* op = (TcpSocket*)arg;
+    while (1)
+    {
+        string msg = op->recvMsg();
+        if (msg.empty())
+        {
+            break;
+        }
+        cout << "recvMsg: " << msg << endl;
+        string sendMsg = "";
+        printf("请输入");
+        cin >> sendMsg;
+        op->sendMsg(sendMsg);
+    }
+    return nullptr;
+}
 int main()
 {
-    printf("%s 向你问好!\n", "SecKeyServer");
+    TcpServer* ser = new TcpServer;
+    ser->setListen(6666);
+    while (1)
+    {
+        cout << "开始accept" << endl;
+        TcpSocket* op = ser->acceptConn();
+        if (op)
+        {
+            pthread_t tid;
+            pthread_create(&tid, NULL, callback, op);
+            pthread_detach(tid);
+        }
+    }
+    
     return 0;
 }
